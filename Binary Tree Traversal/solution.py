17efc09045a3787bf9aca81fcc90b17f6a67a800
LiliAlania <alaniia.pn@ucu.edu.ua>
2025-04-16 16:09:19 +0300

    in_order method

--- Binary Tree Traversal/solution.py
+++ Binary Tree Traversal/solution.py
@@ -8,3 +8,14 @@ def pre_order(node):
     if node.right:
         res += (pre_order(node.right))
     return res
+
+def in_order(node):
+    if node is None:
+        return []
+    res = []
+    if node.left:
+        res += in_order(node.left)
+    res.append(node.data)
+    if node.right:
+        res += in_order(node.right)
+    return res
