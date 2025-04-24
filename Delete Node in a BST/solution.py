fdf0679c685be0c19b47be48e2b44f620815289a
LiliAlania <alaniia.pn@ucu.edu.ua>
2025-04-24 11:04:58 +0300

    Update delete_node

--- Delete Node in a BST/solution.py
+++ Delete Node in a BST/solution.py
@@ -14,3 +14,13 @@ class Solution(object):
         """
         if not root:
             return None
+        res = []
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
