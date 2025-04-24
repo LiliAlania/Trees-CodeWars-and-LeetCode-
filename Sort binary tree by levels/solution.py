3a2d016d6fcc38a53286812cd69d0dcbf9269531
LiliAlania <alaniia.pn@ucu.edu.ua>
2025-04-24 10:30:36 +0300

    adding while cycle to the function

--- Sort binary tree by levels/solution.py
+++ Sort binary tree by levels/solution.py
@@ -4,3 +4,11 @@ def tree_by_levels(node):
         return []
     res = []
     queue = deque([node])
+    while queue:
+        current = queue.popleft()
+        res.append(current.value)
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    return res
